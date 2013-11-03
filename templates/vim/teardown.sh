d3a342ea1c753c48484913eb0abbbd2a46a993cc
Thiago de Arruda <tpadilha84@gmail.com>
2013-11-03 12:48:18 -0300

    Update documentation

--- templates/vim/teardown.sh
+++ templates/vim/teardown.sh
@@ -1,10 +1,8 @@
 # Get the vim pane id
 vim_pane_id=$(geto vim-pane 2> /dev/null)
 
-if [ -n "$vim_pane_id" ]; then
-	# Send escape to ensure vim is in normal mode
-	send -t $vim_pane_id "Escape"
+# Send escape to ensure vim is in normal mode
+send -t $vim_pane_id "Escape"
 
-	# Write all buffers and quit vim
-	cmd -t $vim_pane_id ":wqa"
-fi
+# Write all buffers and quit vim
+cmd -t $vim_pane_id ":wqa"
