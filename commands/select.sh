f2a8f02b109a8ca446fbb99c0b77f7d35b0be45f
Thiago de Arruda <tpadilha84@gmail.com>
2013-11-03 07:10:27 -0300

    Implemented scrolling to select menu

--- commands/select.sh
+++ commands/select.sh
@@ -10,17 +10,26 @@ update() {
 	cls=
 	if [ $# -gt 0 ]; then
 		cls=1
+		tput cup 0 0
 	fi
 	tmp="$sessions"
 	current=0
+	line=0
 
 	while [ ${#tmp} -gt 0 ]; do
 		# move the cursor to the current line
 		e="$(h "$tmp")"
 		tmp="$(t "$tmp")"
+		if [ $current -lt $display_start ]; then
+			current=$((current + 1))
+			continue
+		fi
+		if [ $current -gt $display_end ]; then
+			break
+		fi
 		# Avoid needless screen flickering by only redrawing the updated lines
 		if [ -n "$cls" ] || [ "$current" = "$selected" ] || [ "$current" = "$previous" ]; then
-			tput cup $current 0 # move the cursor to the current line
+			tput cup $line 0    # move the cursor to the current line
 			tput el             # clear to the end of line
 			if [ "$current" = "$selected" ]; then
 				echo "${hilight}-> $e${normal}"
@@ -29,6 +38,7 @@ update() {
 			fi
 		fi
 		current=$((current + 1))
+		line=$((line + 1))
 	done
 }
 # get the numbers of lines/columns
@@ -37,6 +47,8 @@ columns=$(tput cols)
 
 # selected entry index
 selected=0
+display_start=0
+display_end=$((lines - 1))
 
 # sessions sessions
 sessions=""
@@ -57,9 +69,9 @@ echo "$tmp"
 while [ ${#tmp} -gt 0 ]; do
 	e="$(h "$tmp")"
 	tmp="$(t "$tmp")"
-	if [ "$e" = "$last_session" ]; then
-		selected=$count
-	fi
+	# if [ "$e" = "$last_session" ]; then
+	# 	selected=$count
+	# fi
 	# if session is running, add session id separated by colon
 	if tmux has-session -t "$e" > /dev/null 2>&1; then
 		id="$(tmux display -p -t "$e" '#{session_id}')"
@@ -88,17 +100,17 @@ while [ ${#running} -gt 0 ]; do
 	e="$(h "$running")"
 	running="$(t "$running")"
 	id="${e##*:}"
-
-	if [ "$id" = "$running_session_id" ]; then
-		selected=$count
-	fi
-
 	name="${e%:*}"
 	found=
 	for i in $ids; do
 		[ "$i" = "$id" ] && found=1 && break
 	done
 	if [ -z "$found" ]; then
+
+		# if [ "$id" = "$running_session_id" ]; then
+		# 	selected=$count
+		# fi
+
 		stat="(unmanaged, id = $id)"
 		name="$(printf "%-$((columns - 30))s %-25s" "$name" "$stat")"
 		if [ -n "$sessions" ]; then
@@ -136,14 +148,26 @@ while true; do
 			if [ $((selected)) -gt 0 ]; then
 				previous=$selected
 				selected=$((selected - 1))
-				update
+				if [ $selected -lt $display_start ]; then
+					display_start=$((display_start - 1))
+					display_end=$((display_end - 1))
+					update 1
+				else
+					update
+				fi
 			fi
 			;;
 		"$down"|j)
 			if [ $((selected)) -lt $((count - 1)) ]; then
 				previous=$selected
 				selected=$((selected + 1))
-				update
+				if [ $selected -gt $display_end ]; then
+					display_start=$((display_start + 1))
+					display_end=$((display_end + 1))
+					update 1
+				else
+					update
+				fi
 			fi
 			;;
 		"$lf")
