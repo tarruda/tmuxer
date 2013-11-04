5143c7ea9867a38f31f0dad16c70e269bd8d7549
Thiago de Arruda <tpadilha84@gmail.com>
2013-11-04 10:58:22 -0300

    Fix attach command

--- commands/attach.sh
+++ commands/attach.sh
@@ -4,5 +4,5 @@ shorthelp "Attaches to a tmux session or opens the selection menu" "$@"
 
 
 if ! tmux attach > /dev/null 2>&1; then
-	sh "$TMUXER_ROOT/commands/select.sh"
+	sh "$TMUXER_ROOT/commands/choose-session.sh"
 fi
